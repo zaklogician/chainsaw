7fadc075971c9b4c0c03ec1c20e4e77a09c14b19
JerrySwan <dr.jerry.swan@gmail.com>
2016-11-12 16:38:12 +0000

    Move the project to AIMA

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,9 +1,11 @@
 import org.scalacheck.Properties
 import org.scalacheck.Prop
 import org.scalacheck.Gen.{listOf, alphaStr, numChar}
+import aima.core.learning.reinforcement.example._
 
-object TestMnist extends Properties("mnist") {
-  property("actually works") = Prop.apply {
+object TestMnist extends Properties("aima") {
+  property("main") = Prop.apply {
+    aima.LearningDemo.main( Array.empty[String] )
     true
   }
 }
